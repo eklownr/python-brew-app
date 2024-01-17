1a18fce325ee5a649896a8e31856f79c55bf0b6d
eklownr <eklownr@gmail.com>
2024-01-17 20:07:01 +0100

    ttkbootstrap

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ import ttkbootstrap as ttk
 def _alk_vol():
     og = entry_og_int.get()
     fg = entry_fg_int.get()
-    result = (int(og) - int(fg)) * 0.132
-    output_string.set(str(result))
+    result: float = (og - fg) * 0.132
+    round_result = round(result, 2)
+    output_string.set(str(round_result) + " Vol %")
 
 
 # Window
-window = ttk.Window(themename="darkly")
+window = ttk.Window(themename="cyborg")
 window.title("Python-brew-app")
-window.geometry("400x600")
+window.geometry("500x600")
 
 # Title
 tile_lable = ttk.Label(master=window, text="Calculate vol %", font="arial 26 bold", foreground="white", padding=10)
@@ -22,13 +23,17 @@ tile_lable.pack()
 input_frame = ttk.Frame(master=window)
 entry_og_int = tk.IntVar()
 entry_fg_int = tk.IntVar()
+og_lable = ttk.Label(master=input_frame, text="OG: ")
 entry_og = ttk.Entry(master=input_frame, textvariable=entry_og_int)
+fg_lable = ttk.Label(master=input_frame, text="FG: ")
 entry_fg = ttk.Entry(master=input_frame, textvariable=entry_fg_int) 
 button = ttk.Button(master=input_frame, text="Calulate", command=_alk_vol)
 
+og_lable.pack()
+entry_og.pack(pady=20)
+fg_lable.pack()
+entry_fg.pack(pady=20)
 
-entry_og.pack(pady=10)
-entry_fg.pack()
 button.pack(pady=20)
 input_frame.pack()
 
